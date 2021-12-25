6a29e9b343924e8bb09b9f872fb8743e9b23bb78
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-12-25 08:49:23 +0800

    Add f2c
    Changes:
     - enable user to specify a 2nd arg: 'f' or 'c' which determines the calculation to be run
     - add enum type
     - print the conversion implied by the new arg

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,60 @@
 use std::env;
 use std::error::Error;
+use std::str::FromStr;
+
+enum Degrees {
+    C,
+    F,
+}
+
+impl FromStr for Degrees {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Degrees, Self::Err> {
+        match s {
+            "c" => Ok(Degrees::C),
+            "f" => Ok(Degrees::F),
+            _ => Err(()),
+        }
+    }
+}
 
 pub struct Config {
-    temperature_celsius: f64,
+    temperature: f64,
+    degrees: Degrees,
 }
 
 impl Config {
     pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
         args.next();
 
-        let temperature_celsius = match args.next() {
+        let temperature = match args.next() {
             Some(arg) => arg,
             None => {
-                return Err(
-                    "Missing degrees celsius. Please call c2f with a floating point argument",
-                );
+                return Err("Missing temperature. Please call c2f with a floating point argument");
             }
         };
 
-        let temperature_celsius = match temperature_celsius.parse::<f64>() {
+        let temperature = match temperature.parse::<f64>() {
             Ok(f) => f,
             Err(_) => return Err("Could not parse floating point number."),
         };
 
+        let degrees = match args.next() {
+            Some(arg) => arg,
+            None => {
+                return Err("Missing degrees. Please call c2f with a 'c' or 'f'");
+            }
+        };
+
+        let degrees = match Degrees::from_str(&degrees) {
+            Ok(degrees) => degrees,
+            Err(_) => return Err("Wrong degrees. Please call c2f with a 'c' or 'f'"),
+        };
+
         Ok(Config {
-            temperature_celsius,
+            temperature,
+            degrees,
         })
     }
 }
@@ -33,9 +63,15 @@ fn c2f(c: f64) -> f64 {
     c * (9.0 / 5.0) + 32.0
 }
 
+fn f2c(f: f64) -> f64 {
+    (f - 32.0) * (5.0 / 9.0)
+}
+
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
-    let temperature_fahrenheit = c2f(config.temperature_celsius);
-    println!("{}", temperature_fahrenheit);
+    match config.degrees {
+        Degrees::C => println!("{}", c2f(config.temperature)),
+        Degrees::F => println!("{}", f2c(config.temperature)),
+    }
     Ok(())
 }
 
@@ -43,14 +79,20 @@ pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
 mod tests {
     use super::*;
 
-    #[test]
-    fn conversion_works() {
-        const BOILING_POINT_C: f64 = 100.0;
-        const FREEZING_POINT_C: f64 = 0.0;
-        const BOILING_POINT_F: f64 = 212.0;
-        const FREEZING_POINT_F: f64 = 32.0;
+    const BOILING_POINT_C: f64 = 100.0;
+    const FREEZING_POINT_C: f64 = 0.0;
+    const BOILING_POINT_F: f64 = 212.0;
+    const FREEZING_POINT_F: f64 = 32.0;
 
+    #[test]
+    fn c2f_works() {
         assert_eq!(c2f(BOILING_POINT_C), BOILING_POINT_F);
         assert_eq!(c2f(FREEZING_POINT_C), FREEZING_POINT_F);
+    }
+
+    #[test]
+    fn f2c_works() {
+        assert_eq!(BOILING_POINT_C, f2c(BOILING_POINT_F));
+        assert_eq!(FREEZING_POINT_C, f2c(FREEZING_POINT_F));
     }
 }
